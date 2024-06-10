6f2ad67414554f1b77a25ed9f7523527db340327
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2024-06-10 16:34:26 +0200

    small example to avoid empty page syndrome

---
+++ src/example.js
@@ -0,0 +1,13 @@
+import { PetriNet } from './petriNetModel';
+
+export function buildExample() {
+    const exampleNet = new PetriNet();
+
+    exampleNet.addPlace('A', 'A', 1);
+    exampleNet.addPlace('B', 'B', 0);
+    exampleNet.addTransition('T1', 'T1');
+    exampleNet.addArc('A', 'T1', 1);
+    exampleNet.addArc('T1', 'B', 1);
+
+    return exampleNet;
+}
