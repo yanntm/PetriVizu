ddb56560ac879fabfc20ca8d1a7c8cff843c0af4
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2024-06-10 14:10:46 +0200

    our server code

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 1664;
+
+// Serve the static files from the "website" directory
+app.use(express.static(path.join(__dirname, 'website')));
+
+// Route to serve index.html by default
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'website', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
